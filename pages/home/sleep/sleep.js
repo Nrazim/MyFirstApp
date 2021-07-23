14b80a5714e788fd766ea7e773aa5e696fe0d304
DONG423 <2523213163@qq.com>
2021-07-23 14:43:39 +0800

    qwe

--- pages/home/sleep/sleep.js
+++ pages/home/sleep/sleep.js
@@ -2,6 +2,7 @@
 const app=getApp();
 const AV = require('../../../libs/av-core-min.js');
 var util = require('../../../utils/util.js');
+var definedTime = 2030;
 
 Page({
 
@@ -14,6 +15,10 @@ Page({
     ],
     slimeaction:"https://www.z4a.net/images/2021/07/17/_x264.gif",
     timeStart: util.formatTime(new Date()),
+    dialogShow1: false,
+    dialogShow2: false,
+    buttons1:[{text:'还是算了'}, {text:'教我做事？'}],
+    buttons2:[{text:'再等等'}, {text:'睡！少废话'}]
   },
   click: function (e) {
     var currentUser = AV.User.current();
@@ -32,7 +37,7 @@ Page({
     var etime = Date.parse(timeEnd)
     var sleepDuration = (etime - stime)/1000
     if(sleepDuration>1800){
-      sleepTime.set('sleepDuration',sleepDuration)
+      sleepTime.set('sleepDuration',sleepDuration/60)
       sleepTime.set('parent',currentUser)
       sleepTime.set('sleepStart',this.data.timeStart)
       sleepTime.set('sleepEnd',timeEnd)
@@ -49,17 +54,65 @@ Page({
     currentUser.save();
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    const currentUser = AV.User.current();
-    if(!currentUser.attributes.isSleeping){
+  tapDialogButton1(e){
+    var currentUser=AV.User.current()
+    this.setData({
+      dialogShow1: false,
+    })
+    if(e.detail.index==0){
+      wx.redirectTo({
+        url: '../index/index',
+      })
+    }
+    else{
       this.setData({
         timeStart:util.formatTime(new Date())
       })
       currentUser.set("isSleeping",this.data.timeStart)
       currentUser.save();
+    }
+  },
+  
+  tapDialogButton2(e){
+    var currentUser=AV.User.current()
+    this.setData({
+      dialogShow2: false,
+    })
+    if(e.detail.index==0){
+      wx.redirectTo({
+        url: '../index/index',
+      })
+    }
+    else{
+      this.setData({
+        timeStart:util.formatTime(new Date())
+      })
+      currentUser.set("isSleeping",this.data.timeStart)
+      currentUser.save();
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const currentUser = AV.User.current();
+    var myDate = new Date()
+    var myHour = myDate.getHours()
+    var myMinute = myDate.getMinutes()
+    var myTime = myHour*100+myMinute
+    console.log(myTime)
+    if(!currentUser.attributes.isSleeping){  //如果不在睡觉
+      if (myTime<definedTime){  //如果入睡时间过早
+        this.setData({          //进入判断
+          dialogShow1: true
+        })
+      }
+      else{
+        this.setData({
+          dialogShow2: true
+        })
+      }
     }
     else{
       this.setData({
