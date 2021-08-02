10e1daa8dfec8da40b36370ff4c518630cc4773a
Nrazim <978178916@qq.com>
2021-08-02 16:52:41 +0800

    no userinfo

--- pages/home/index/index.js
+++ pages/home/index/index.js
@@ -195,17 +195,4 @@ Page({
       exp: Math.round(app.globalData.exp/app.globalData.levelexplist[app.globalData.level]*100),
     })
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
 })
